77c9c3e9c58170e118d6a6236569ee433a1462a2
maciej-szopa <maciej.szopa.14@ucl.ac.uk>
2016-03-24 15:59:54 +0100

    Changed the Poem class

--- app/src/main/java/com/example/szopa/poetry/Poem.java
+++ app/src/main/java/com/example/szopa/poetry/Poem.java
@@ -5,42 +5,21 @@ package com.example.szopa.poetry;
  */
 public class Poem {
     private Source1 aSource1 = new Source1();
-    private String poem = "";
     private String source = aSource1.getSource();
 
     public java.lang.String writePoem(int numberOfWords, int numberOfLines, int numberOfParagraphs) {
-        // split the big source string into an array of word-length strings
-        // (one or more space marks the splitting boundary)
-        String[] sourceToWords = source.split("\\s+");
-        for (int i = 0; i < sourceToWords.length; i++) {
-            sourceToWords[i] = sourceToWords[i].replaceAll("[^\\w]", ""); // delete all non-word characters (. - , etc.)
-        }
 
-        // two loops for the sake of clarity
-        // count all uppercase words in array
-        int l = 0;
-        for (int j = 0; j < sourceToWords.length; j++) {
-            if (Character.isUpperCase(sourceToWords[j].charAt(0))) {
-                l++;
-            }
-        }
+        String[] sourceToWords = splitIntoWords(source);
+        String[] uppercaseList = prepareUppercaseList(sourceToWords);
+        String poem = "";
 
-        String[] upperCaseList = new String [l];
-        // search for all uppercase words and add them to a separate array
-        int m = 0;
-        for (int j = 0; j < sourceToWords.length; j++) {
-            if (Character.isUpperCase(sourceToWords[j].charAt(0))) {
-                upperCaseList[m] = sourceToWords[j];
-                m++;
-            }
-        }
-
-        // print a poem using random elements from the array sourceToWords[]
+        // print (concatenate) a poem using random elements from the array sourceToWords[]
         for (int i = 0; i < numberOfParagraphs; i++) {
             for (int j = 0; j < numberOfLines; j++) {
+                // first word is from the array of uppercase words
                 if(numberOfWords > 0) {
-                    int randomInitialWord = (int) (Math.random() * upperCaseList.length);
-                    poem += upperCaseList[randomInitialWord] + " ";
+                    int randomInitialWord = (int) (Math.random() * uppercaseList.length);
+                    poem += uppercaseList[randomInitialWord] + " ";
                 }
                 for (int k = 0; k < numberOfWords - 1; k++) {
                     int randomWord = (int) (Math.random() * sourceToWords.length);
@@ -52,6 +31,42 @@ public class Poem {
         }
 
         return poem;
+    }
+
+    private String [] splitIntoWords(String input){
+        // split the big source string into an array of word-length strings
+        // (one or more space marks the splitting boundary)
+        String[] splitString = input.split("\\s+");
+
+        // delete all non-word characters (. - , etc.)
+        for (int i = 0; i < splitString.length; i++) {
+            splitString[i] = splitString[i].replaceAll("[^\\w]", "");
+        }
+
+        return splitString;
+    }
+
+
+    private String[] prepareUppercaseList(String[] upperAndLowercaseList) {
+        // count all uppercase words in array
+        int l = 0;
+        for (int j = 0; j < upperAndLowercaseList.length; j++) {
+            if (Character.isUpperCase(upperAndLowercaseList[j].charAt(0))) {
+                l++;
+            }
+        }
+
+        String[] upperCaseList = new String [l];
+        // search for all uppercase words and add them to a separate array
+        int m = 0;
+        for (int j = 0; j < upperAndLowercaseList.length; j++) {
+            if (Character.isUpperCase(upperAndLowercaseList[j].charAt(0))) {
+                upperCaseList[m] = upperAndLowercaseList[j];
+                m++;
+            }
+        }
+
+        return upperCaseList;
     }
 
     }
